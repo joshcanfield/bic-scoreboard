9ab8ed51a373ac523470adab0e7483cd92ae2ff6
Josh Canfield <jcanfield@zulily.com>
2015-05-29 12:52:50 -0700

    Fixed bat file to run on windows xp

--- src/main/dist/bin/run.bat
+++ src/main/dist/bin/run.bat
@@ -1,18 +1,25 @@
-@echo off
+REM @echo off
+cd %~dp0
+cd ..
+set INSTALL_DIR=%cd%
+cd ..
+set BASE_DIR=%cd%
 
-set INSTALL_DIR=c:/scoreboard-1.1
+set JAVA_HOME=%BASE_DIR%/jre1.8.0_45
 cd %INSTALL_DIR%
 
 tasklist /FI "IMAGENAME eq java.exe" 2>NUL | find /I /N "java.exe">NUL
 if "%ERRORLEVEL%"=="0" goto START_CHROME
 
 echo "Starting scoreboard server!"
-start "Scoreboard Server" /min java -Dlogback.configurationFile=conf/logback.xml ^
+start "Scoreboard Server" /min %JAVA_HOME%\bin\java ^
+   -Dlogback.configurationFile=conf/logback.xml ^
+   -Dscoreboard.commport=COM4 ^
    -cp %INSTALL_DIR%/lib/*;%INSTALL_DIR%/scoreboard-1.1.jar;%INSTALL_DIR%/conf/ ^
    canfield.bia.ServiceMain start
 
-@sleep while the service starts up...
-timeout 4
+echo "sleep while the service starts up..."
+ping -n 5 127.0.0.1 >nul
 
 :START_CHROME
 start chrome "http://localhost:8080"
